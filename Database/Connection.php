0c58b9b85b26debb2540035e98be722c3dfc92f1
sidalto <sidalto.pj@gmail.com>
2021-10-31 19:28:30 -0300

    feat(database): cria conexão com o banco de dados

---
+++ Database/Connection.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Trilha01\Database;
+
+use PDO;
+use PDOException;
+
+class Connection
+{
+    private const HOST = "localhost";
+    private const DB_NAME = "wjcrypto";
+    private const USER = "root";
+    private const PASSWORD = "triger";
+
+    private const OPTIONS = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+    ];
+
+    private static $instance;
+
+    final private function __construct()
+    {
+    }
+
+    /** @return PDO|PDOException */
+    final public function getInstance(): PDO
+    {
+        try {
+            if (empty(self::$instance)) {
+                self::$instance = new PDO(
+                    "mysql:host=" . self::HOST . ";dbname=" . self::DB_NAME,
+                    self::USER,
+                    self::PASSWORD,
+                    self::OPTIONS
+                );
+            }
+
+            return self::$instance;
+        } catch (PDOException $exception) {
+            return $exception;
+        }
+    }
+}
